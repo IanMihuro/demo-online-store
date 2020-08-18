0e6685ba441ab025b2840e2fe5ba316046b99625
Ian Mihuro <ianmihuro@Ians-MacBook-Pro.local>
2020-08-18 19:12:51 +0300

    update  menu-itemscomponent to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,16 +47,8 @@ class Directory extends Component {
     const { sections } = this.state;
     return (
       <div className="directory-menu">
-        {sections.map(({ title, imageUrl, size, id, linkUrl }) => {
-          return (
-            <MenuItem
-              key={id}
-              title={title}
-              imageUrl={imageUrl}
-              size={size}
-              linkUrl={linkUrl}
-            />
-          );
+        {sections.map(({ id, ...otherSectionProps }) => {
+          return <MenuItem key={id} {...otherSectionProps} />;
         })}
       </div>
     );
